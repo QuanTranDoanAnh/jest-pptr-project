88ac7f64d26b1cb17604deb158bf00b5a4984035
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-09-30 10:43:43 +0900

    Framework part 2 - try to login

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,11 +1,17 @@
 import HomePage from '../pages/HomePage'
+import LoginPage from '../pages/LoginPage'
+import TopBar from '..//pages/components/TopBar'
 
 describe('Example', () => {
   let homepage
+  let loginPage
+  let topBar
 
   beforeAll(async () => {
     jest.setTimeout(15000)
     homepage = new HomePage()
+    loginPage = new LoginPage()
+    topBar = new TopBar()
   })
 
   it('homepage should work', async () => {
@@ -13,6 +19,14 @@ describe('Example', () => {
   })
 
   it('navbar should be displayed', async () => {
-    await homepage.visit()
+    await homepage.isNavbarDisplayed()
+    await topBar.isTopBarDisplayed()
+  })
+
+  it('try to login', async () => {
+    await loginPage.visit()
+    await loginPage.isLoginFormDisplayed()
+    await loginPage.login("username", "password")
+    await loginPage.wait(5000)
   })
 })

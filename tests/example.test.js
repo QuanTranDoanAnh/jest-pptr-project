938685f8002a7441dd1d74d16e56dddee7f8fd97
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-09-30 11:02:55 +0900

    Framework 4 - feedback form

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,16 +1,19 @@
 import HomePage from '../pages/HomePage'
 import LoginPage from '../pages/LoginPage'
+import FeedbackPage from '../pages/FeedbackPage'
 import TopBar from '..//pages/components/TopBar'
 
 describe('Example', () => {
   let homepage
   let loginPage
+  let feedbackPage
   let topBar
 
   beforeAll(async () => {
     jest.setTimeout(15000)
     homepage = new HomePage()
     loginPage = new LoginPage()
+    feedbackPage = new FeedbackPage()
     topBar = new TopBar()
   })
 
@@ -28,5 +31,16 @@ describe('Example', () => {
     await loginPage.isLoginFormDisplayed()
     await loginPage.login("username", "password")
     await loginPage.wait(5000)
-  })
+  }, 15000)
+
+  it('feedback should work', async () => {
+    await feedbackPage.visit()
+    await feedbackPage.isFeedbackFormDisplayed()
+    await feedbackPage.submitFeedback(
+      "QuanTDA1", 
+      "td.anhquan@gmail.com",
+      "Test Feedback Form",
+      "Feedback Form should work")
+    await feedbackPage.wait(5000)
+  }, 15000)
 })
